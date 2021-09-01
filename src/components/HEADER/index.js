75efd18dfa2028b9175a19606c25307319cf7906
Kamal Nanda <kamalnanda45@gmail.com>
2021-09-01 13:37:49 +0530

    Update index.js

--- src/components/HEADER/index.js
+++ src/components/HEADER/index.js
@@ -11,7 +11,11 @@ const MobileHeader = () => {
 	}
 	return ( 
 		<div id="mobile-nav">
-			<img alt="ham" onClick={toggleDropDown} id="ham" src={isShown ? "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/VisualEditor_-_Icon_-_Close_-_white.svg/1200px-VisualEditor_-_Icon_-_Close_-_white.svg.png" : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Hamburger_icon_white.svg/1024px-Hamburger_icon_white.svg.png"} />
+			<div style={{display:'flex', justifyContent:'space-between', alignItems:'center', width:'100%'}}>
+				<img style={{width:'160px'}} class="header-logo" src={logo} alt="logo" /> 
+				<img alt="ham" onClick={toggleDropDown} id="ham" src={isShown ? "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/VisualEditor_-_Icon_-_Close_-_white.svg/1200px-VisualEditor_-_Icon_-_Close_-_white.svg.png" : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Hamburger_icon_white.svg/1024px-Hamburger_icon_white.svg.png"} />
+			</div>
+			
 			{
 				isShown && <ul> 
 			    <li> 
