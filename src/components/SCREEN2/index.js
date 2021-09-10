f1c0bc4e86e1cdbf9913bd0349b45772fa4cc287
KamalNanda <kamalnanda45@gmail.com>
2021-09-11 01:38:28 +0530

    new updates

--- src/components/SCREEN2/index.js
+++ src/components/SCREEN2/index.js
@@ -3,86 +3,57 @@ import './style.css'
 import PinkBox from '../Common/PinkBox'
 import Lineup1 from '../../assets/images/lineup1.png'
 import Lineup2 from '../../assets/images/lineup2.png'
-import Lineup3 from '../../assets/images/lineup3.png'
-import REDX from '../../assets/images/red-x.png'
+import Lineup3 from '../../assets/images/lineup3.png' 
 import Cieling from '../../assets/images/CAVE CIELING FRONT.png'
-import Group from '../../assets/images/Group 383.png'
-import box from '../../assets/images/Group 388.png'
-import coin from '../../assets/images/coin.png'
-import plus from '../../assets/images/+.png'
-import equal from '../../assets/images/=.png'
+import box from '../../assets/images/Group 388.png'  
 import arrow from '../../assets/images/pink-arrow.png'
-import heart from '../../assets/images/heart.png'
-import Discord from '../../assets/images/DiscordScreen.png' 
-import DiscordMobile from '../../assets/images/discord-mobile.png' 
+import heart from '../../assets/images/heart.png' 
+import s2l from '../../assets/images/s2-left.png' 
+import s2r from '../../assets/images/s2-right.png'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Group from '../../assets/images/Group 383.png'
 const Screen = () => {
-	const lists = ["Du$t is a token you can mine by chatting in any NFT community on Discord that hosts a Magic Du$t Miner Bot.", "The more you chat the more Du$t you collect.", "Magic Du$t Miners are NFTs that multiply the Du$t you collect." ]
-	const lists2 = ["Only 11,111 Miners exist, but there’s no limit to how many Miners you can hold in a single wallet. ", "Each Magic Du$t Miner you hold will further multiply your $Du$t rewards!"]
-	const minersList = [[Lineup1] , [Lineup1, plus, Lineup2], [Lineup1, plus, Lineup2, plus, Lineup3] ]
+	const list1=['When you hodl a Magic Du$t Miner in your NFT wallet every chat, like, emoji and so on is rewarded with Du$t!','Magic Du$t Miners are NFTs that multiply the Du$t you collect. ']
+	const list2=['Du$t is a pretty sweet ERC-20 token based on the Wax and Polygon blockchains. ','Each Magic Du$t Miner you hold will further multiply your $Du$t rewards!  ','You can mine $Dust by chatting in any NFT community on Discord that hosts a Magic Du$t Miner Bot.','Only 11,111 Miners exist, but there’s no limit to how many Miners you can hold in a single wallet. ']
+	
 	return(<div style={{position:'relative'}}>
 			<img src={Cieling} class="cieling" /> 
 		<div id="section-1"> 
 				<Row>
-					<Col lg={5} md={5} sm={12}> 
-						<div className="pink-boxes-grid">
-							<div className="pink-box">
-								<h1>Magic Du$t Miners help you mine Du$t Tokens</h1>
-							</div>
-							{
-								lists.map((l, i) => {
-									return <PinkBox key={i} > 
-											<div class="d-flex"><img src={arrow} className="circle-arrow"/><p>{l}</p></div>
-									</PinkBox>
-								})
-							}
-							<div className="m-show">
-								{
-									lists2.map((l, i) => {
-										return <PinkBox key={i} > 
-												<div class="d-flex"><img src={arrow} className="circle-arrow"/><p>{l}</p></div>
-										</PinkBox>
-									})
-								}
-							</div>
-						</div> 
-					</Col> 
-					<Col lg={7} md={7} sm={12} className="sec-2-col">
-						<img src={Group} id="group"/>
-							<PinkBox> 
-							{
-								minersList.map((mine, i) => {
-									return <div key={i} class='d-flex my-2 align-items-center'>
-												{
-													mine.map((img, j) =>{
-														return <img key={j} alt={`miner-${i}-${j}`} src={img} className={ img == plus ? 'add-white' : 'pink-box-img'} />
-													})
-												}
-												<img src={equal} className="add-white" />  
-												{
-													mine.map((img, j) =>{
-														if(img == plus) return <></>
-														else return <img className="mx-1 coin" key={j} alt={`coin-${j}`}  src={coin} />
-													})
-												}
-											</div> 
-								})
-							} 
-						</PinkBox> 
-						<div className="m-hide pink-boxes-grid" style={{position:'relative', zIndex:'-1'}}>
-							{
-								lists2.map((l, i) => {
-									return <PinkBox key={i} > 
-											<div class="d-flex"><img src={heart} className="circle-arrow"/><p>{l}</p></div>
-									</PinkBox>
-								})
-							}
-						</div>
+					<Col lg={12}>
+						<div className="pink-box">
+							<h1>Magic Du$t Miners are hardworking NFTs that mine Du$t Tokens for you when you chat in the Magic Dust Discord Community</h1>
+						</div>		
 					</Col>
-				</Row>
-				<img src={Discord} className="m-hide mx-auto my-5 discord-wall" />
-				<img src={DiscordMobile} className="m-show mx-auto my-5 discord-wall" />
+					{
+						list1.map((l, i) => {
+							return <Col lg={6} md={6} sm={12}>
+										<PinkBox key={i} > 
+											<div class="d-flex"><img src={heart} className="heart"/><p>{l}</p></div>
+										</PinkBox>
+									</Col>
+						})
+					}
+					<Row className="justify-content-between align-items-center">
+						<Col lg={8} md={8} sm={12}>
+							<img style={{width:'100%'}} src={s2l} alt="s2l" />
+						</Col>
+						<Col lg={4} md={4} sm={12}>
+							<img style={{width:'100%'}} src={s2r} alt="s2r" />
+						</Col>
+					</Row>
+					{
+						list2.map((l, i) => {
+							return <Col lg={6} md={6} sm={12}>
+										<PinkBox key={i} > 
+											<div class="d-flex"><img src={heart} className="heart"/><p>{l}</p></div>
+										</PinkBox>
+									</Col>
+						})
+					}
+					
+				</Row> 
 
 		</div>
 	</div>)
--- src/components/SCREEN3/index.js
+++ src/components/SCREEN3/index.js
@@ -7,15 +7,29 @@ import topps from '../../assets/images/partners/topps.png'
 import bayc from '../../assets/images/partners/bayc.png' 
 import nw from '../../assets/images/partners/nw.png'  
 import btc from '../../assets/images/partners/btc.png' 
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Group from '../../assets/images/Group 383.png'
+import dialogue from '../../assets/images/dialogue.png'
 const Screen = () => { 
 
 	return(<div style={{position:"relative"}}>
 		<div id="section-2">
 			{/** <img src={Man} /> **/}
-	        <p>Some of the biggest NFT<br/>
-				 communities are mining $dust<br/>
-				 on Discord + Telegram . . .
-			</p>
+			<Row className="align-items-end">
+				<Col lg={4} md={4} sm={12}>
+					<img src={Group} />
+				</Col> 
+				<Col lg={8} md={8} sm={12}>
+					<div style={{position:'relative'}}>
+						<img className="miner-dialogue" src={dialogue} />
+					</div>
+			        <p>Some of the biggest NFT<br/>
+						 communities are mining $dust<br/>
+						 on Discord + Telegram . . .
+					</p>
+				</Col>
+			</Row>
             <div class="games-flex" style={{marginTop:'100px'}}>
 				<img className="mt-3" src={btc} /> 
 				<img className="mt-3" src={nw} /> 
